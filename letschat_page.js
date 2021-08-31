a2e0d7e0a64a0aa28f467561963ea8f189837978
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-08-31 16:30:46 +0530

    Update letschat_page.js

--- letschat_page.js
+++ letschat_page.js
@@ -1,15 +1,15 @@
-var firebaseConfig = {
-      apiKey: "AIzaSyARVNqY2S7rxAHE2y_-hXyspPSrBiWc55Y",
-      authDomain: "chatapp-28e10.firebaseapp.com",
-      databaseURL: "https://chatapp-28e10.firebaseio.com",
-      projectId: "chatapp-28e10",
-      storageBucket: "chatapp-28e10.appspot.com",
-      messagingSenderId: "937361435899",
-      appId: "1:937361435899:web:cb6f913fe1277d7382945e",
-      measurementId: "G-43PBYJWVQN"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCVWrQYrHjze_2rVDNuARf8eecp115Uq9U",
+  authDomain: "chatweb-53865.firebaseapp.com",
+  databaseURL: "https://chatweb-53865-default-rtdb.firebaseio.com",
+  projectId: "chatweb-53865",
+  storageBucket: "chatweb-53865.appspot.com",
+  messagingSenderId: "889379280424",
+  appId: "1:889379280424:web:1275d6886e7a5e4864071d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
     roomname=localStorage.getItem("rn");
     console.log(roomname);
     username=localStorage.getItem("username");
